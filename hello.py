d43484849d1340ac87ee48cd8f995e125ad33f65
jagga <jg4611@gmail.com>
2022-08-27 21:57:05 +0530

    database added to details too

--- hello.py
+++ hello.py
@@ -11,15 +11,14 @@ app = Flask(__name__)
 # create a secret key
 app.config['SECRET_KEY'] = 'mysupersecretkey'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
-app.config['SQLALCHEMY_BINDS'] = {'hobbies': 'sqlite:///hobbies.db'}
+app.config['SQLALCHEMY_BINDS'] = {
+    'hobbies': 'sqlite:///hobbies.db', 'details': 'sqlite:///details.db'}
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = 'True'
 
 # initialise the db
 db = SQLAlchemy(app)
 
 # create Users model
-
-
 class Users(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(200), nullable=False)
@@ -31,8 +30,6 @@ class Users(db.Model):
         return '<Name %r>' % self.name
 
 # create Hobby model
-
-
 class Hobby(db.Model):
     __bind_key__ = 'hobbies'
     id = db.Column(db.Integer, primary_key=True)
@@ -45,32 +42,44 @@ class Hobby(db.Model):
     def __repr__(self):
         return '<Name %r>' % self.name
 
-# create a form class
-
-
+# create Details model
+class Details(db.Model):
+    __bind_key__ = 'details'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200), nullable=False)
+    email = db.Column(db.String(200), nullable=False, unique=True)
+    address = db.Column(db.String(200), nullable=False)
+    date_added = db.Column(db.DateTime, default=datetime.utcnow)
+    # create a string
+    def __repr__(self):
+        return '<Name %r>' % self.name
+    
+# create a form Namer class
 class NamerForm(FlaskForm):
     name = StringField("What's your name: ", validators=[DataRequired()])
     email = StringField("What's your email: ", validators=[DataRequired()])
     address = StringField("What's your address: ", validators=[DataRequired()])
     submit = SubmitField("submit")
 
-# create another form class
-
-
+# create another form Hobby class
 class HobbyForm(FlaskForm):
     name = StringField("Name: ", validators=[DataRequired()])
     email = StringField("Email: ", validators=[DataRequired()])
     hobby = StringField("Hobby: ", validators=[DataRequired()])
     submit = SubmitField("submit")
 
-# create user form
-
-
+# create user form class
 class UserForm(FlaskForm):
     name = StringField("Name ", validators=[DataRequired()])
     email = StringField("Email ", validators=[DataRequired()])
     submit = SubmitField("submit")
 
+# create Details form class
+class DetailsForm(FlaskForm):
+    name = StringField("What's your name: ", validators=[DataRequired()])
+    email = StringField("What's your email: ", validators=[DataRequired()])
+    address = StringField("What's your address: ", validators=[DataRequired()])
+    submit = SubmitField("submit")
 
 # create add_user route and function
 @app.route('/user/add', methods=['GET', 'POST'])
@@ -91,8 +100,6 @@ def add_user():
     return render_template("add_user.html", form=form, name=name, our_users=our_users)
 
 # create add_hobby route and function
-
-
 @app.route('/hobby/add', methods=['GET', 'POST'])
 def add_hobby():
     name = None
@@ -116,6 +123,29 @@ def add_hobby():
     hobby_lists = Hobby.query.order_by(Hobby.date_added)
     return render_template("add_hobby.html", form=form, name=name, email=email, hobby=hobby, hobby_lists=hobby_lists)
 
+# create add_details route and function
+@app.route('/details/add', methods=['GET', 'POST'])
+def add_details():
+    name = None
+    email = None
+    address = None
+    form = DetailsForm()
+    if form.validate_on_submit():
+        detail = Details.query.filter_by(name=form.name.data).first()
+        if detail is None:
+            detail = Details(name=form.name.data,
+                          email=form.email.data, address=form.address.data)
+            db.session.add(detail)
+            db.session.commit()
+        name = form.name.data
+        email = form.email.data
+        address = form.address.data
+        form.name.data = ''
+        form.email.data = ''
+        form.address.data = ''
+        flash('Detail added successfully')
+    detail_lists = Details.query.order_by(Details.date_added)
+    return render_template("add_details.html", form=form, name=name, email=email, address=address, detail_lists=detail_lists)
 # define another form route
 
 
@@ -142,8 +172,8 @@ def hobby():
 def details():
     name = None
     email = None
-    address=None
-    form = NamerForm()
+    address = None
+    form = DetailsForm()
     # validate form
     if form.validate_on_submit():
         flash('Congrats! Your basic details are now added to our database.')
@@ -152,7 +182,7 @@ def details():
         address = form.address.data
         form.name.data = ''
         form.email.data = ''
-        form.address.data=''
+        form.address.data = ''
     return render_template("details.html", form=form, name=name, email=email, address=address)
 
 
